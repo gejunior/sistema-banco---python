9640553e22aa1109379e9a544412230ab6179840
genilson <genilson_junior14@outlook.com>
2025-06-02 21:01:50 -0300

    finalizado 2

--- sistema_bancario.py
+++ sistema_bancario.py
@@ -22,3 +22,85 @@
 # No fim da listagem deve ser exibido o saldo atual da conta.
 #Os valores devem ser exibidos utilizando o formato R$ xxx.xx, exemplo:
 #1500.45 = R$ 1500.45
+
+saldo = 0
+saques = []
+saque_diario = 0
+depositos = []
+
+def depositar(valor):
+    global saldo
+    if valor > 0.0:
+        saldo += valor
+        depositos.append(valor)
+
+def sacar():
+    global saque_diario, saldo, saques
+    if saque_diario >= 3:
+        print("Limite de saque diario atingido!")
+        return
+    else:
+        valor = float(input("Digite o valor do saque: R$ "))
+        
+        if valor < 0:
+            print("Valor inválido para saque.")
+            return
+    
+        elif valor > saldo or saldo <= 0:
+            print("Saldo insuficiente.")
+            return
+
+        elif valor > 500:
+            print("Limite de apenas R$ 500,00")
+            return
+        else:
+            print(f"Saque de {valor:.2f} realizado com sucesso!")
+            saque_diario += 1
+            saques.append(valor)
+            saldo -= valor
+
+def extrato():
+    print("Extrato:".center(15, "="))
+    print("Depósitos:")
+    for deposito in depositos:
+        print(f"R$ {deposito:.2f}")
+    print("Saques:")
+    for saque in saques:
+        print(f"R$ {saque:.2f}")
+    print(f"Saldo atual: R$ {saldo:.2f}")
+
+def menu():
+    op = 0
+    print("Menu".center(15))
+    print("[1] depositar")
+    print("[2] Sacar")
+    print("[3] Extrato")
+    print("[0] Sair")
+    op = int(input("Opcao: "))
+    return op
+
+def main():
+    print("Bem-vindo ao sistema bancário!")
+    
+    while True:
+        op = menu()
+        match op:
+            case 1:
+                valor = float(input("Digite o valor do depósito: R$ "))
+                depositar(valor)
+            case 2:
+                sacar()
+            case 3:
+                extrato()
+            case 0:
+                print("Saindo do sistema...")
+                break
+            case _:
+                print("Opção inválida, tente novamente.")
+    print("Obrigado por usar nosso sistema bancário!")
+
+main()
+# if __name__ == "__main__":
+    # main()
+    
+
